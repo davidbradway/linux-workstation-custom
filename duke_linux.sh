853730fc17f6e937573824aa4964890239ef5475
David Bradway <david.bradway@gmail.com>
2015-01-24 12:15:50 -0500

    add commands to setup repos and preferences

---
+++ duke_linux.sh
@@ -0,0 +1,106 @@
+#Setup gnome-terminal-colors-monokai
+git clone git://github.com/pricco/gnome-terminal-colors-monokai.git
+cd gnome-terminal-colors-monokai
+./install.sh
+
+#Setup github repos
+cd ~
+mkdir repos
+cd /getlab/dpb6/repos/
+git clone git@github.com:Duke-Ultrasound/ultratrack.git
+git clone git@github.com:Duke-Ultrasound/SC2000.git
+git clone git@github.com:Duke-Ultrasound/fem.git
+git clone git@github.com:Duke-Ultrasound/colormaps.git
+git clone git@github.com:Duke-Ultrasound/gpuBF.git
+git clone git@github.com:Duke-Ultrasound/IM.git
+git clone git@github.com:Duke-Ultrasound/Field_II.git
+git clone git@github.com:Duke-Ultrasound/messina.git
+git clone git@github.com:Duke-Ultrasound/URI.git
+git clone git@github.com:Duke-Ultrasound/beamer_template.git
+git clone git@github.com:Duke-Ultrasound/mex.git
+git clone git@github.com:Duke-Ultrasound/ProcessArfiData.git
+git clone git@github.com:Duke-Ultrasound/arfi_code.git
+
+subllime_text ~/.bashrc
+#alias dropbox='/scratch/dpb6/.dropbox-dist/dropboxd &'
+#alias sub='sublime_text &'
+#alias sublime='sublime_text &'
+#
+#alias cd..='cd ..'
+#alias cp='cp -i'
+#alias d='ls'
+#alias df='df -h -x supermount'
+#alias du='du -h'
+#alias egrep='egrep --color'
+#alias fgrep='fgrep --color'
+#alias grep='grep --color'
+#alias l='ls'
+#alias la='ls -a'
+#alias ll='ls -l'
+#alias ls='ls -F --color=auto'
+#alias lsd='ls -d */'
+#alias md='mkdir'
+#alias mv='mv -i'
+#alias p='cd -'
+#alias rd='rmdir'
+#alias rm='rm -i'
+
+# UPDATE ALL REPOS
+cd /getlab/dpb6/repos/animation_scripts
+git pull
+cd /getlab/dpb6/repos/calcsws
+git pull
+cd /getlab/dpb6/repos/dtucode
+git pull
+cd /getlab/dpb6/repos/fullwave
+git pull
+cd /getlab/dpb6/repos/IM
+git pull
+cd /getlab/dpb6/repos/nickbot
+git pull
+cd /getlab/dpb6/repos/S2000
+git pull
+cd /getlab/dpb6/repos/Slicer
+git pull
+cd /getlab/dpb6/repos/SSA_rs232_rev5
+git pull
+cd /getlab/dpb6/repos/URI
+git pull
+cd /getlab/dpb6/repos/arfi_code
+git pull
+cd /getlab/dpb6/repos/code
+git pull
+cd /getlab/dpb6/repos/fem
+git pull
+cd /getlab/dpb6/repos/fullwave-python
+git pull
+cd /getlab/dpb6/repos/messina
+git pull
+cd /getlab/dpb6/repos/ProcessArfiData
+git pull
+cd /getlab/dpb6/repos/SC2000
+git pull
+cd /getlab/dpb6/repos/SLSC
+git pull
+cd /getlab/dpb6/repos/verasonics
+git pull
+cd /getlab/dpb6/repos/beamer_template
+git pull
+cd /getlab/dpb6/repos/colormaps
+git pull
+cd /getlab/dpb6/repos/Field_II
+git pull
+cd /getlab/dpb6/repos/gpuBF
+git pull
+cd /getlab/dpb6/repos/mex
+git pull
+cd /getlab/dpb6/repos/qiba
+git pull
+cd /getlab/dpb6/repos/sge
+git pull
+cd /getlab/dpb6/repos/ssa_motion
+git pull
+cd /getlab/dpb6/repos/ultratrack
+git pull
+cd ~
+
