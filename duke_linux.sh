4ca8199d54bc50c35d25a3570ed2375765323d0f
David Bradway <david.bradway@gmail.com>
2015-01-24 22:42:20 -0500

    update repos

--- duke_linux.sh
+++ duke_linux.sh
@@ -1,27 +1,27 @@
 #Setup gnome-terminal-colors-monokai
-git clone git://github.com/pricco/gnome-terminal-colors-monokai.git
-cd gnome-terminal-colors-monokai
-./install.sh
+#git clone git://github.com/pricco/gnome-terminal-colors-monokai.git
+#cd gnome-terminal-colors-monokai
+#./install.sh
 
 #Setup github repos
 cd ~
 mkdir repos
 cd /getlab/dpb6/repos/
-git clone git@github.com:Duke-Ultrasound/ultratrack.git
+#git clone git@github.com:Duke-Ultrasound/ultratrack.git
 git clone git@github.com:Duke-Ultrasound/SC2000.git
 git clone git@github.com:Duke-Ultrasound/fem.git
 git clone git@github.com:Duke-Ultrasound/colormaps.git
-git clone git@github.com:Duke-Ultrasound/gpuBF.git
-git clone git@github.com:Duke-Ultrasound/IM.git
+#git clone git@github.com:Duke-Ultrasound/gpuBF.git
+#git clone git@github.com:Duke-Ultrasound/IM.git
 git clone git@github.com:Duke-Ultrasound/Field_II.git
 git clone git@github.com:Duke-Ultrasound/messina.git
 git clone git@github.com:Duke-Ultrasound/URI.git
-git clone git@github.com:Duke-Ultrasound/beamer_template.git
-git clone git@github.com:Duke-Ultrasound/mex.git
+#git clone git@github.com:Duke-Ultrasound/beamer_template.git
+#git clone git@github.com:Duke-Ultrasound/mex.git
 git clone git@github.com:Duke-Ultrasound/ProcessArfiData.git
 git clone git@github.com:Duke-Ultrasound/arfi_code.git
 
-subllime_text ~/.bashrc
+#subllime_text ~/.bashrc
 #alias dropbox='/scratch/dpb6/.dropbox-dist/dropboxd &'
 #alias sub='sublime_text &'
 #alias sublime='sublime_text &'
@@ -46,16 +46,16 @@ subllime_text ~/.bashrc
 #alias rm='rm -i'
 
 # UPDATE ALL REPOS
-cd /getlab/dpb6/repos/animation_scripts
-git pull
-cd /getlab/dpb6/repos/calcsws
-git pull
+#cd /getlab/dpb6/repos/animation_scripts
+#git pull
+#cd /getlab/dpb6/repos/calcsws
+#git pull
 cd /getlab/dpb6/repos/dtucode
 git pull
 cd /getlab/dpb6/repos/fullwave
 git pull
-cd /getlab/dpb6/repos/IM
-git pull
+#cd /getlab/dpb6/repos/IM
+#git pull
 cd /getlab/dpb6/repos/nickbot
 git pull
 cd /getlab/dpb6/repos/S2000
@@ -84,18 +84,18 @@ cd /getlab/dpb6/repos/SLSC
 git pull
 cd /getlab/dpb6/repos/verasonics
 git pull
-cd /getlab/dpb6/repos/beamer_template
-git pull
+#cd /getlab/dpb6/repos/beamer_template
+#git pull
 cd /getlab/dpb6/repos/colormaps
 git pull
 cd /getlab/dpb6/repos/Field_II
 git pull
-cd /getlab/dpb6/repos/gpuBF
-git pull
-cd /getlab/dpb6/repos/mex
-git pull
-cd /getlab/dpb6/repos/qiba
-git pull
+#cd /getlab/dpb6/repos/gpuBF
+#git pull
+#cd /getlab/dpb6/repos/mex
+#git pull
+#cd /getlab/dpb6/repos/qiba
+#git pull
 cd /getlab/dpb6/repos/sge
 git pull
 cd /getlab/dpb6/repos/ssa_motion
